22e087e37e072d8adce232b4a63c035f4039ea55
2ADA2 <artemada2@gmail.com>
2025-07-19 21:40:11 +0300

    changable

---
+++ js/downloadPdf.js
@@ -0,0 +1,25 @@
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+
+export async function downloadPDF() {
+    const pdf = new jsPDF({
+        orientation: "portrait",
+        unit: "mm",
+        format: "a4"
+    });
+
+    const element = document.getElementById("cv");
+
+    const canvas = await html2canvas(element, {
+        scale: 2
+    });
+
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = pdf.internal.pageSize.getHeight();
+
+    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+
+    pdf.save("odna-stranica.pdf");
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,55 @@
 import '../css/style.css'
+import {downloadPDF} from "./downloadPdf.js";
+
+const downloader = document.getElementById('pdf-downloader');
+
+downloader.addEventListener('click', (e) => {
+    downloadPDF();
+})
+
+
+document.addEventListener("click", (e) => {
+    let target = e.target
+    if (target.classList.contains('changeable')) {
+        const modalForm = document.getElementById('modal-form');
+        const modal = document.getElementById('modal');
+        const modalBackground = document.getElementById('modal-background');
+        const cancelButton = document.getElementById('cancel-button');
+        const newText = document.getElementById('new-text');
+
+
+        modal.className = "background-modal-show modal"
+        modalBackground.className = "modal-background-show"
+        newText.value = target.innerText;
+        newText.focus()
+
+        document.addEventListener("keydown", (e) => {
+            if(e.key === "Escape"){
+                closeModal();
+            }
+        })
+
+        document.body.style.overflow = "hidden"
+
+        cancelButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            closeModal();
+        })
+
+        modalForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            target.innerText = newText.value
+            closeModal();
+        })
+
+        function closeModal() {
+            modalBackground.className = "background-modal-hidden";
+            modal.className = "modal-hidden"
+            document.body.style.overflow = "auto"
+        }
+
+    }
+})
+
+
 
